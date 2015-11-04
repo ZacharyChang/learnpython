18110bd9b2ead8d03a8b01aa75b39e86ac54630b
Zachary <zacharychang.zc@gmail.com>
2015-11-04 23:23:32 +0800

    add access restrict to the Student class

--- student.py
+++ student.py
@@ -1,15 +1,30 @@
 class Student(object):
     def __init__(self,name,score):
-        self.name=name
-        self.score=score
+        self.__name=name
+        self.__score=score
+
+    def get_name(self):
+        return self.__name
+
+    def get_score(self):
+        return self.__score
+
+    def set_name(self,name):
+        self.__name=name
+
+    def set_score(self,score):
+        if 0<= score <= 100:
+            self.__score=score
+        else:
+            raise ValueError('wrong value of score')
 
     def print_score(self):
-        print('%s: %s' % (self.name,self.score))
+        print('%s: %s' % (self.__name,self.__score))
 
     def get_grade(self):
-        if self.score>= 90:
+        if self.__score>= 90:
             return 'A'
-        elif self.score>=60:
+        elif self.__score>=60:
             return 'B'
         else:
             return 'C'
@@ -17,3 +32,8 @@ class Student(object):
 bart=Student('Bart Smith',80)
 bart.print_score()
 print(bart.get_grade())
+bart.set_name('Jon Snow')
+bart.set_score(92)
+print(bart.get_name())
+print(bart.get_score())
+bart.print_score()
