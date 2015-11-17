9f9a67b950964add139ae4bd94872eb7a4e38c7c
Zachary <zacharychang.zc@gmail.com>
2015-11-17 23:18:31 +0800

    add something about writing in file

--- io_file.py
+++ io_file.py
@@ -1,2 +1,17 @@
-f=open('c:/pytest.txt')
-print(f.read())
+with open('\pytest.txt','w') as f1:
+    f1.write('Hello,Zachary!')
+'''
+try:
+    f=open('c:/pytest.txt','r')
+    print(f.read())
+finally:
+    if f:
+        f.close()
+'''
+#same as
+#with open('\pytest.txt','r',encoding='gbk',errors='ingore')
+with open('\pytest.txt','r') as f2: #'rb'读二进制 
+    for line in f2.readlines():
+        print(line.strip())     #去掉默认'\n'
+
+
