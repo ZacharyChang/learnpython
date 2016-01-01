e0a92e32f1c2a7f46bd33378db3d08c78b0affb7
Zachary <zacharychang.zc@gmail.com>
2016-01-01 23:42:47 +0800

    search and others

--- rexx/rexx.py
+++ rexx/rexx.py
@@ -1,6 +1,7 @@
 __author__ = 'ZacharyChang'
 
 import re
+# from re import findall,search,sub,S
 
 # example of .
 a = 'aba123'
@@ -30,3 +31,20 @@ xx34awfaftjlkjaxxworldxxafda2342'''
 # re.S makes . match new line
 f = re.findall('xx(.*?)xx', secret2, re.S)
 print(f)
+
+# search
+s2 = 'asdfxxIxx123xxlovexxdfd'
+g1 = re.search('xx(.*?)xx123xx(.*?)xx', s2).group(1)
+g2 = re.search('xx(.*?)xx123xx(.*?)xx', s2).group(2)
+g3 = re.findall('xx(.*?)xx123xx(.*?)xx', s2)
+print(g1)
+print(g2)
+print(g3[0][1])
+
+# sub
+h = re.sub('xx(.*?)xx', '123%s456' % 'new', s2)
+print(h)
+
+s3 = 'afdasfg2332daadgdfs32423ad'
+i = re.findall('(\d+)', s3)
+print(i)
