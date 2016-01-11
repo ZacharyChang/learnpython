67e77cf33a07081e23506034cb1281ca0da26b6a
Zachary <zacharychang.zc@gmail.com>
2016-01-11 19:45:38 +0800

    complete the spider for zerodm

--- xPath_learn/spider_zerodm.py
+++ xPath_learn/spider_zerodm.py
@@ -2,7 +2,6 @@
 from lxml import etree
 from multiprocessing.dummy import Pool as ThreadPool
 import requests
-import json
 
 
 def spider(url):
@@ -11,37 +10,37 @@ def spider(url):
     content_field = selector.xpath('//dl[@class="l-list mt10 oh"]/dd/ul/li')
     item = {}
     for each in content_field:
-        name = each.xpath('a[@class="zerowzlh"]/text()')
-        print(name)
-        # author = reply_info['author']['user_name']
-        # content = \
-        #     each.xpath(
-        #             'div[@class="d_post_content_main"]/div/cc/div[@class="d_post_content j_d_post_content  clearfix"]/text()')[
-        #         0]
-        # reply_time = reply_info['content']['date']
-        # print(content)
-        # print(reply_time)
-        # print(author)
+        name = each.xpath('a[@class="zerowzlh"]/@title')[0]
+        url = each.xpath('a[@class="zerowzlh"]/@href')[0]
+        episod = each.xpath('div[@class="dhp"]/span[@class="set"]/text()')[0]
+        type = each.xpath('p[1]/text()')[0]
+        click = each.xpath('p[2]/text()')[0]
+
         item['name'] = name
-        # item['topic_reply_content'] = content
-        # item['topic_reply_time'] = reply_time
+        item['url'] = 'http://dmxz.zerodm.com' + url
+        item['episod'] = episod
+        item['type'] = type[3:]
+        item['click'] = click[3:-1]
+
         towrite(item)
 
 
 def towrite(contentdict):
-    f.writelines(u'名称:' + str(contentdict['name']) + '\n')
+    f.writelines(u'名称: ' + str(contentdict['name']) + '\n')
+    f.writelines(u'地址: ' + str(contentdict['url']) + '\n')
+    f.writelines(u'集数: ' + str(contentdict['episod']) + '\n')
+    f.writelines(u'类别: ' + str(contentdict['type']) + '\n')
+    f.writelines(u'点击: ' + str(contentdict['click']) + '\n')
+    f.writelines('\n')
 
-
-#     f.writelines(u'类别:' + str(contentdict['topic_reply_content']) + '\n')
-#     f.writelines(u'点击:' + contentdict['user_name'] + '\n\n')
 
 if __name__ == '__main__':
     pool = ThreadPool(4)
     f = open('zerodm.txt', 'a', encoding='utf-8')
     page = []
-    for i in range(0, 20):
-        newpage = 'http://dmxz.zerodm.com/list/wanjie/' + str(i)
-        print("处理 " + newpage + " 中...")
+    for i in range(0, 81):
+        newpage = 'http://dmxz.zerodm.com/list/1/' + str(i)
+        print("添加 " + newpage + " 中...")
         page.append(newpage)
 
     results = pool.map(spider, page)
